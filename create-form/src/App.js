edf26cc28c5f397a073502ef9b8d54ae609d1499
amruta-nejkar <amruta.nejkar@successive.com>
2022-01-11 18:55:07 +0530

    createform

---
+++ create-form/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StudentComponent from './components/studentCompent';
+
+function App() {
+  return (
+   <>
+   <StudentComponent/>
+   </>
+  );
+}
+
+export default App;
